74479147133f93345a3a7de9f8a3ec75a50af140
selma <belhadjselma1@gmail.com>
2023-03-02 16:52:13 +0100

    Fixed ruby offences

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,34 +1,33 @@
 class ApplicationController < ActionController::API
   before_action :authorized
-  
-  secret_key = 'simihammas123'
+
+  @secret_key = 'simihammas123'
   def encode_token(payload)
-    JWT.encode(payload, secret_key)
+    JWT.encode(payload, @secret_key)
   end
 
   def decoded_token
     header = request.headers['Authorization']
-    if header 
-      token = header.split(' ')[1]
-      begin
-        JWT.decode(token, secret_key, true, algorithm: 'HS256')
-      rescue  JWT::DecodeError
-        nil
-      end
+    return unless header
+
+    token = header.split[1]
+    begin
+      JWT.decode(token, @secret_key, true, algorithm: 'HS256')
+    rescue JWT::DecodeError
+      nil
     end
   end
 
-  def current_user 
-    if decoded_token
-        user_id = decoded_token[0]['user_id']
-        @user = User.find_by(id: user_id)
-    end
+  def current_user
+    return unless decoded_token
+
+    user_id = decoded_token[0]['user_id']
+    @user = User.find_by(id: user_id)
   end
 
   def authorized
-    if !current_user
-      render json: { message: 'Please log in' }, status: :unauthorized
-    end
-  end
+    return if current_user
 
+    render json: { message: 'Please log in' }, status: :unauthorized
+  end
 end

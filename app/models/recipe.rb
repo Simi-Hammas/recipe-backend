2b3eb95668195badf96e8f1b56b41189db63f831
HAMMAS-SALEEM <hammassaleem376@gmail.com>
2023-05-04 17:49:48 +0500

    :wheel_of_dharma: Add Associations for all models

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,9 +1,9 @@
 class Recipe < ApplicationRecord
-    belongs_to :user
-    has_many :likes
-    has_many :comments
-    has_many :saved_recipes
-    has_many :rankings
-    has_many :recipe_ingredients
-
+  belongs_to :user
+  has_many :likes
+  has_many :comments
+  has_many :saved_recipes
+  has_many :rankings
+  has_many :recipe_ingredients
+  has_many :ingredients, through: :recipe_ingredients
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
+  has_many :recipes
+  has_many :likes
+  has_many :comments
+  has_many :saved_recipes
+  has_many :rankings
+  has_many :ingredients
+
   has_secure_password
   validates :username, uniqueness: true
 end

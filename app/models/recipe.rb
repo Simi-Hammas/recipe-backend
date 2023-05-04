22ffc07e78cf4122418c052382b314320d13847a
selma <belhadjselma1@gmail.com>
2023-05-04 13:26:39 +0100

    Create associations for recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,9 @@
 class Recipe < ApplicationRecord
+    belongs_to :user
+    has_many :likes
+    has_many :comments
+    has_many :saved_recipes
+    has_many :rankings
+    has_many :recipe_ingredients
+
 end

89c15c45a9721aa7392cb3d9d04dfca84b1a9b6f
HAMMAS-SALEEM <hammassaleem376@gmail.com>
2023-05-04 20:16:22 +0500

    :ambulance: Fix the deleted_recipe validation in recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -10,12 +10,12 @@ class Recipe < ApplicationRecord
   validates :title, presence: true, length: { minimum: 4, maximum: 200 }
   validates :description, presence: true, length: { minimum: 15 }
   validates :method, presence: true, length: { minimum: 15 }
-  validates :preparation_time, presence: true, numericality: { only_integer: true, less_than_or_equal_to: 4 }
-  validates :cooking_time, presence: true, numericality: { only_integer: true, less_than_or_equal_to: 4 }
+  validates :preparation_time, presence: true, numericality: { only_integer: true, less_than_or_equal_to: 9999 }
+  validates :cooking_time, presence: true, numericality: { only_integer: true, less_than_or_equal_to: 9999 }
   validates :ease_of_preparation, presence: true
   validates :photo, presence: true
   validates :recipe_type, presence: true
   validates :country, presence: true, format: { with: /[a-zA-Z]/ }, length: { minimum: 4, maximum: 50 }
-  validates :accessible, presence: true
-  validates :deleted_recipe, presence: true
+  validates :accessible, inclusion: [true, false]
+  validates :deleted_recipe, inclusion: [true, false]
 end

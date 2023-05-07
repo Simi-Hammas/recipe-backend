fe82f0567103dcd794c55b07eef168dee22c7d58
selma <belhadjselma1@gmail.com>
2023-05-07 17:16:57 +0100

    Updated the ingredient model with validations

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,9 @@
 class Ingredient < ApplicationRecord
   has_many :recipe_ingredients
   has_many :recipes, through: :recipe_ingredients
+
+  validates :name, presence: true, length: { minimum: 3, maximum: 200 }, format: { with: /\A[a-zA-Z0-9\s]+\z/, message: "only allows letters, numbers, and spaces" }
+  # validates :photo, presence: true
+  validates :description, length: { minimum: 15 }
+
 end

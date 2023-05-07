4399639ebf1276619bf39320abfb622c93445b20
selma <belhadjselma1@gmail.com>
2023-05-07 17:23:44 +0100

    Updated the recipe_ingredient model with validations

--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -1,4 +1,9 @@
 class RecipeIngredient < ApplicationRecord
   belongs_to :recipe
   belongs_to :ingredient
+
+  validates :quantity, presence: true, length: {maximum: 50, minimum:1}, format: { with: /\A[a-zA-Z0-9\s,./]+\z/, message: "only allows letters, numbers, and spaces and slash" }
+  validates :unit, presence: true, length: { minimum: 1, maximum: 50 } 
+  validates :optional, inclusion: [true, false]
+
 end

738464a7164aac54463aa442de5aad9d93de53e5
selma <belhadjselma1@gmail.com>
2023-05-08 16:14:10 +0100

    Updated the regular expressions for ingredient and user models

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -3,7 +3,7 @@ class Ingredient < ApplicationRecord
   has_many :recipes, through: :recipe_ingredients
 
   validates :name, presence: true, length: { minimum: 3, maximum: 200 },
-                   format: { with: /\A[a-zA-Z0-9\s]+\z/, message: 'only allows letters, numbers, and spaces' }
+                   format: { with: /\A[a-zA-Z0-9\s-]+\z/, message: 'only allows letters, numbers, and spaces' }
   # validates :photo, presence: true
   validates :description, length: { minimum: 15 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,11 +8,13 @@ class User < ApplicationRecord
 
   has_secure_password
   validates :username, uniqueness: true, length: { minimum: 3, maximum: 200 },
-                       format: { with: /\A[a-zA-Z0-9\s]+\z/, message: 'only allows letters, numbers, and spaces' }
+                       format: { with: /\A[a-zA-Z0-9\s\-_]+\z/, message: 'only allows letters, numbers,\
+                         spaces, dashes, and underscores' }
   validates :email, presence: true, uniqueness: true, length: { maximum: 255 },
                     format: { with: URI::MailTo::EMAIL_REGEXP, message: 'must be a valid email address' }
   validates :country, presence: true, length: { minimum: 3, maximum: 200 }
   validates :public_name, presence: true, length: { minimum: 3, maximum: 200 },
-                          format: { with: /\A[a-zA-Z0-9\s]+\z/, message: 'only allows letters, numbers, and spaces' }
+                          format: { with: /\A[a-zA-Z0-9\s\-_]+\z/, message: 'only allows letters, numbers,\
+    spaces, dashes, and underscores' }
   validates :bio, length: { minimum: 1, maximum: 3500 }
 end

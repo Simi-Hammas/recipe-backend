0d93931d165333697405a94302446b60fc9dbee2
HAMMAS-SALEEM <hammassaleem376@gmail.com>
2023-05-08 18:59:53 +0500

    :ambulance: Fix some linter offenses

--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -2,8 +2,7 @@ class RecipeIngredient < ApplicationRecord
   belongs_to :recipe
   belongs_to :ingredient
 
-  validates :quantity, presence: true, length: {maximum: 50, minimum:1}, format: { with: /\A[a-zA-Z0-9\s,./]+\z/, message: "only allows letters, numbers, and spaces and slash" }
-  validates :unit, presence: true, length: { minimum: 1, maximum: 50 } 
+  validates :quantity, presence: true, length: { maximum: 50, minimum: 1 }
+  validates :unit, presence: true, length: { minimum: 1, maximum: 50 }
   validates :optional, inclusion: [true, false]
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,12 @@ class User < ApplicationRecord
   has_many :ingredients
 
   has_secure_password
-  validates :username, uniqueness: true, length: { minimum: 3, maximum: 200 }, format: { with: /\A[a-zA-Z0-9\s]+\z/, message: "only allows letters, numbers, and spaces" }
-  validates :email, presence: true, uniqueness: true ,length: { maximum: 255 }, format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }
+  validates :username, uniqueness: true, length: { minimum: 3, maximum: 200 },
+                       format: { with: /\A[a-zA-Z0-9\s]+\z/, message: 'only allows letters, numbers, and spaces' }
+  validates :email, presence: true, uniqueness: true, length: { maximum: 255 },
+                    format: { with: URI::MailTo::EMAIL_REGEXP, message: 'must be a valid email address' }
   validates :country, presence: true, length: { minimum: 3, maximum: 200 }
-  validates :public_name, presence: true, length: { minimum: 3, maximum: 200 }, format: { with: /\A[a-zA-Z0-9\s]+\z/, message: "only allows letters, numbers, and spaces" }
+  validates :public_name, presence: true, length: { minimum: 3, maximum: 200 },
+                          format: { with: /\A[a-zA-Z0-9\s]+\z/, message: 'only allows letters, numbers, and spaces' }
   validates :bio, length: { minimum: 1, maximum: 3500 }
 end

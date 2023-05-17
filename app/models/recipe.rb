290aa99b3a45402ed4585d4c804e1e5ea4821b3f
selma <belhadjselma1@gmail.com>
2023-05-17 16:56:45 +0100

    Updated the recipe model: validations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -7,15 +7,22 @@ class Recipe < ApplicationRecord
   has_many :recipe_ingredients
   has_many :ingredients, through: :recipe_ingredients
 
-  validates :title, presence: true, length: { minimum: 4, maximum: 200 }
+  validates :title, presence: true, length: { minimum: 3, maximum: 200 }
   validates :description, presence: true, length: { minimum: 15 }
   validates :method, presence: true, length: { minimum: 15 }
-  validates :preparation_time, presence: true, numericality: { only_integer: true, less_than_or_equal_to: 9999 }
-  validates :cooking_time, presence: true, numericality: { only_integer: true, less_than_or_equal_to: 9999 }
+  validates :preparation_time, presence: true,
+                               numericality: { only_integer: true, less_than_or_equal_to: 9999,
+                                               greater_than: 0 }
+  validates :cooking_time, presence: true,
+                           numericality: { only_integer: true, less_than_or_equal_to: 9999,
+                                           greater_than_or_equal_to: 0 }
   validates :ease_of_preparation, presence: true
   validates :photo, presence: true
   validates :recipe_type, presence: true
-  validates :country, presence: true, format: { with: /[a-zA-Z]/ }, length: { minimum: 4, maximum: 50 }
+  validates :country, presence: true, format: { with: /\A[A-Za-z]+(?: [A-Za-z]+)?\z/ },
+                      length: { minimum: 3, maximum: 50 }
   validates :accessible, inclusion: [true, false]
   validates :deleted_recipe, inclusion: [true, false]
+  validates :likes_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :comments_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
